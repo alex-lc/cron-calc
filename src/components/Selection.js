daf58ba7a5a3cbf80427f5ba0ea0c2a81af7910c
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2021-03-18 21:13:11 -0500

    added react-hook-form and began building cron selection form with it. I ran into design decision conflict though. going to work on something else while I think it over.

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,8 +1,12 @@
 // Input for setting up cron job
 
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Selection = () => {
+
+    // react-hook-form
+    const { register, error, handleSubmit } = useForm()
 
     const [cron, setCron] = useState({
         minute: 0,
@@ -17,16 +21,33 @@ const Selection = () => {
             ...cron,
             [e.target.name]: e.target.value
         })
-    } 
+    }
+
+    const submitCron = (data) => {
+        setCron({
+            ...cron,
+            data
+        })
+    }
     
     return (
         <div>
             <div>
-                <input type="text" name="minute" onChange={updateValue} value={cron.minute} />
-                <input type="text" name="hour" onChange={updateValue} value={cron.hour} />
-                <input type="text" name="dayOfTheMonth" onChange={updateValue} value={cron.dayOfTheMonth} />
-                <input type="text" name="month" onChange={updateValue} value={cron.month} />
-                <input type="text" name="dayOfTheWeek" onChange={updateValue} value={cron.dayOfTheWeek} />
+                <div>
+                    {cron.minute === 0 && <p>0 minutes</p>}
+                    {cron.minute !== 0 && <p>{cron.minute} minutes</p>}
+
+                    {cron.hour === 0 && <p>0 hour</p>}
+                    {cron.hour !== 0 && <p>{cron.hour} hour</p>}
+                </div>
+                <form onSubmit={handleSubmit(submitCron)}>
+                    <input type="text" name="minute" ref={register({ required: true })} />
+                    <input type="text" name="hour" ref={register({ required: true })} />
+                    <input type="text" name="dayOfTheMonth" ref={register({ required: true })} />
+                    <input type="text" name="month" ref={register({ required: true })} />
+                    <input type="text" name="dayOfTheWeek" ref={register({ required: true })} />
+                    <button type="submit"></button>
+                </form>
             </div>
         </div>
     )
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,12 @@
 /* Validate input when user is selecting cron job */
 
 function validateMinutes(minutes) {
+
+    let allowedStringValues = ["*", "/", ","]
+    
     // only wildcard should be accepted as a string
-    if (typeof minutes === "string" && minutes !== "*") {
-        return false
+    if (typeof minutes === "string") {
+        // string value combos
     }
     // numbers should be between 0 and 59 inclusive
     else if (typeof minutes === "number" && (minutes < 0 || minutes > 59)) {
