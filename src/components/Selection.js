38503a8070381e324fd55a640d34d1fb7940d4a6
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2021-04-27 15:56:35 -0500

    Simple mistake, just forgot to conditionally render for month

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,6 +2,9 @@
 
 import { useState } from 'react'
 
+// data
+import { months } from '../data/data'
+
 const Selection = () => {
 
     const [cron, setCron] = useState({
@@ -13,10 +16,37 @@ const Selection = () => {
     })
 
     const updateValue = (e) => {
-        setCron({
-            ...cron,
-            [e.target.name]: e.target.value
-        })
+        if (e.target.name === "minute") {
+            console.log("Editing minute field...")
+
+            if (e.target.value > 59) {
+                console.log("Invalid value. It cannot be greater than 59.")
+                setCron({
+                    ...cron,
+                    [e.target.name]: e.target.value
+                })
+            }
+            else {
+                console.log("Valid minute value: " + e.target.value)
+            }
+        }
+
+        if (e.target.name === "month" && (e.target.value <= 12 && e.target.value >= 1)) {
+            console.log("Valid month. Grabbing month...")
+            console.log(`You selected ${months[e.target.value]}!`)
+
+            setCron({
+                ...cron,
+                [e.target.name]: e.target.value
+            })
+        }
+        else {
+            console.log("Invalid month. It must be between 1 and 12.")
+        }
+        // setCron({
+        //     ...cron,
+        //     [e.target.name]: e.target.value
+        // })
     }
 
     const submitCron = (data) => {
@@ -30,18 +60,27 @@ const Selection = () => {
         <div>
             <div>
                 <div>
-                    {cron.minute === 0 && <p>0 minutes</p>}
+                    {cron.minute === 0 && <p>0 minute</p>}
                     {cron.minute !== 0 && <p>{cron.minute} minutes</p>}
 
                     {cron.hour === 0 && <p>0 hour</p>}
                     {cron.hour !== 0 && <p>{cron.hour} hour</p>}
+
+                    {cron.dayOfTheMonth === 0 && <p>no day of the month</p>}
+                    {cron.dayOfTheMonth !== 0 && <p>{cron.dayOfTheMonth}</p>}
+
+                    {cron.month === 0 && <p>no month selected</p>}
+                    {cron.month !== 0 && <p>{cron.month}</p>}
+
+                    {cron.dayOfTheWeek === 0 && <p>no day of the week</p>}
+                    {cron.dayOfTheWeek !== 0 && <p>{cron.dayOfTheWeek}</p>}
                 </div>
                 <form>
-                    <input type="text" name="minute" placeholder="*" />
-                    <input type="text" name="hour" placeholder="*" />
-                    <input type="text" name="dayOfTheMonth" placeholder="*" />
-                    <input type="text" name="month" placeholder="*" />
-                    <input type="text" name="dayOfTheWeek" placeholder="*" />
+                    <input type="text" name="minute" onChange={updateValue} placeholder="*" />
+                    <input type="text" name="hour" onChange={updateValue} placeholder="*" />
+                    <input type="text" name="dayOfTheMonth" onChange={updateValue} placeholder="*" />
+                    <input type="text" name="month" onChange={updateValue} placeholder="*" />
+                    <input type="text" name="dayOfTheWeek" onChange={updateValue} placeholder="*" />
                     <button type="submit">CRON</button>
                 </form>
             </div>
