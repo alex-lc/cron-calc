7726767bfa30d1fd7a487e0017efbaf69b36022b
al <al@NowSecures-MBP.attlocal.net>
2021-06-05 21:06:19 -0500

    A little more progress on rendering cron.

--- src/components/Cron.js
+++ src/components/Cron.js
@@ -1,44 +1,72 @@
+import { useState, useEffect } from 'react'
 
 export default function Cron({ minute, hour, dayOfTheMonth, month, dayOfTheWeek }) {
-    if (minute !== "*" && hour !== "*") {
-        return (
-            <div>
-                {hour}:{minute}
 
-                {dayOfTheMonth === "*" ? ` every day` : ` on day of month ${dayOfTheMonth}`}
+    const [minuteString, setMinuteString] = useState("")
+    const [hourString, setHourString] = useState("")
+    const [dayOfTheMonthString, setDayOfTheMonthString] = useState("")
+    const [monthString, setMonthString] = useState("")
+    const [dayOfTheWeekString, setDayOfTheWeekString] = useState("")
 
-                {dayOfTheWeek === "*" ? null : ` and on ${dayOfTheWeek}`}
-
-                {month === "*" ? ` of every month` : ` in ${month}`}
-
-            </div>
-        )
-    }
-    else if (minute === "*" || hour === "*") {
-        if (minute !== "*" && hour === "*") {
-            return (
-                <div>
-                    At minute {minute} on day of month {dayOfTheMonth} and on {dayOfTheWeek} in {month}
-                </div>
-            )
+    useEffect(() => {
+        if (dayOfTheMonth === "*") {
+            setDayOfTheMonthString(" every day")
+        }
+        else {
+            setDayOfTheMonthString(` on day of the month ${dayOfTheMonth}`)
+        }
+    
+        if (dayOfTheWeek === "*") {
+            setDayOfTheWeekString("")
+        }
+        else {
+            setDayOfTheWeekString(` and on ${dayOfTheWeek}`)
+        }
+    
+        if (month === "*") {
+            setMonthString(" of every month")
+        }
+        else {
+            setMonthString(` in ${month}`)
         }
 
         if (minute === "*" && hour !== "*") {
+            setHourString(` hour ${hour}`)
+        }
+
+        if (hour === "*" && minute !== "*") {
+            setMinuteString(`At minute ${minute}`)
+        }
+    }, [minute, hour, dayOfTheMonth, dayOfTheWeek, month])
+
+    if (minute === "*" || hour === "*") {
+        if (minute !== "*" && hour === "*") {
             return (
                 <div>
-                    Every minute past hour {hour} on day of month {dayOfTheMonth} and on {dayOfTheWeek} in {month}
+                    <p>At minute {minute} {dayOfTheMonthString} {dayOfTheWeekString} {monthString}</p>
+                </div>
+            )
+        }
+        else if (minute === "*" && hour !== "*") {
+            return (
+                <div>
+                    <p>At every minute past {hourString} {dayOfTheMonthString} {dayOfTheWeekString} {monthString}</p> 
                 </div>
             )
         }
     }
 
-    if (minute === "*" && hour === "*" && dayOfTheMonth === "*" && month === "*" && dayOfTheWeek === "*") {
-        return <p>Every minute</p>
+    if (minute === "*" && hour === "*" && dayOfTheMonth === "*" && dayOfTheWeek === "*" && month === "*") {
+        return (
+            <div>
+                <p>Every minute</p>
+            </div>
+        )
     }
 
     return (
         <div>
-            {minute} {hour} {dayOfTheMonth} {month} {dayOfTheWeek}
+            <p>{minuteString} {hourString} {dayOfTheMonthString} {monthString} {dayOfTheWeekString} {monthString}</p>
         </div>
     )
 }
